78a814e44099134fb976875d18d6014521e39883
Hyan Carlos Rodrigues da Silva <hyansilva177@gmail.com>
2025-02-17 22:53:57 -0300

    Add files via upload

---
+++ rasfoo_corrigido/rasfoo_corrigido/rasfood/src/main/java/br/com/rasmoo/restaurante/dao/CardapioDao.java
@@ -0,0 +1,61 @@
+package br.com.rasmoo.restaurante.dao;
+
+import br.com.rasmoo.restaurante.entity.Cardapio;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CardapioDao {
+
+    private EntityManager entityManager;
+
+    public CardapioDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    /*
+     * CRUD
+     * C = CREATE
+     * R = READ
+     * U = UPDATE
+     * D = DELETE
+     * */
+
+    public void cadastrar(final Cardapio cardapio) {
+        this.entityManager.persist(cardapio);
+    }
+
+    public Cardapio consultarPorId(final Integer id) {
+        return this.entityManager.find(Cardapio.class, id);
+    }
+
+    public List<Cardapio> consultarPorValor(final BigDecimal filtro){
+        String jpql = "SELECT c FROM Cardapio c WHERE c.valor = :valor";
+        return this.entityManager.createQuery(jpql,Cardapio.class).setParameter("valor",filtro).getResultList();
+    }
+
+    public List<Cardapio> consultarTodos(){
+        String jpql = "SELECT c FROM Cardapio c";
+        return this.entityManager.createQuery(jpql,Cardapio.class).getResultList();
+    }
+
+    public Cardapio consultarPorNome (final String filtro) {
+        try {
+            String jpql = "SELECT c FROM Cardapio c WHERE UPPER (c.nome) = UPPER (:nome)";
+            return this.entityManager.createQuery(jpql, Cardapio.class).setParameter("nome", filtro).getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public void atualizar(final Cardapio cardapio){
+        this.entityManager.merge(cardapio);
+    }
+
+    public void excluir(final Cardapio cardapio){
+        this.entityManager.remove(cardapio);
+    }
+
+}
---
+++ rasfoo_corrigido/rasfoo_corrigido/rasfood/src/main/java/br/com/rasmoo/restaurante/service/teste/CardapioService.java
@@ -0,0 +1,21 @@
+package br.com.rasmoo.restaurante.service.teste;
+
+import br.com.rasmoo.restaurante.dao.CardapioDao;
+import br.com.rasmoo.restaurante.util.CargaDeDadosUtil;
+import br.com.rasmoo.restaurante.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class CardapioService {
+    public static void main(String[] args) {
+        EntityManager entityManager = JPAUtil.getEntityManagerRasFood();
+        entityManager.getTransaction().begin();
+        CargaDeDadosUtil.cadastrarCategorias(entityManager);
+        CargaDeDadosUtil.cadastrarProdutosCardapio(entityManager);
+        CardapioDao cardapioDao = new CardapioDao(entityManager);
+        System.out.println("O valor do produto pesquisado foi: " + cardapioDao.consultarPorValor(BigDecimal.valueOf(47.00)));
+        System.out.println("O produto pesquisado foi: " + cardapioDao.consultarPorNome("Moqueca"));
+        entityManager.close();
+    }
+}
